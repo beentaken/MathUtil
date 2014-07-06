eb62710fd62dab74427f414f447ddb7b21520bb0
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2014-07-06 18:01:54 +0200

    Refactored code

--- MathUtil.Tests/Program.cs
+++ MathUtil.Tests/Program.cs
@@ -37,26 +37,7 @@ namespace MathUtil.Tests
 
         #endregion
 
-        public double Length()
-        {
-            return Math.Sqrt(this.X * this.X + this.Y * this.Y);
-        }
-
-        public double LengthSquared()
-        {
-            return this.X * this.X + this.Y * this.Y;
-        }
-
-        public Vector2 Normalize()
-        {
-            double length = this.Length();
-            return this / length;
-        }
-
-        public double Dot(Vector2 v)
-        {
-            return (this.X * v.X) + (this.Y * v.Y);
-        }
+        #region Arithmetic
 
         public Vector2 Substract(Vector2 v)
         {
@@ -100,6 +81,29 @@ namespace MathUtil.Tests
         public static Vector2 operator /(Vector2 v, double scalar)
         {
             return v.Divide(scalar);
+        }
+
+        public double Dot(Vector2 v)
+        {
+            return (this.X * v.X) + (this.Y * v.Y);
+        }
+
+        #endregion
+
+        public double Length()
+        {
+            return Math.Sqrt(this.X * this.X + this.Y * this.Y);
+        }
+
+        public double LengthSquared()
+        {
+            return this.X * this.X + this.Y * this.Y;
+        }
+
+        public Vector2 Normalize()
+        {
+            double length = this.Length();
+            return this / length;
         }
 
         public Vector2 Project(Vector2 v)
@@ -247,184 +251,47 @@ namespace MathUtil.Tests
 
         static void Main(string[] args)
         {
+            //TestSubstract();
+            //TestAdd();
+            //TestMultiply();
+            //BenchMultiply();
+            //TestDivide();
             //TestDot();
             //BenchDot();
 
-            //TestMultiply();
-            //BenchMultiply();
-
+            //TestLength();
+            //TestLengthSquared();
+            //TestNormalize();
             //TestProject();
             //BenchProject();
 
-            //TestSubstract();
-
-            //TestAdd();
-
-            //TestNearestPointOnLine();
-
-            //TestDistance();
-
-            //TestMiddle();
-
-            //TestLength();
-            //TestLengthSquared();
-
-            //TestDivide();
-
-            //TestNormalize();
-
             //TestLerp();
-
             //TestSlerp();
             //BenchSlerp();
-
             //TestNlerp();
             //BenchNlerp();
+
+            //TestDistance();
+            //TestMiddle();
+            //TestNearestPointOnLine();
 
             Console.ReadLine();
         }
 
-        static void TestNlerp()
+        static void TestSubstract()
         {
-            var v1 = new Vector2(1.0, 0.0);
-            var v2 = new Vector2(0.0, 1.0);
-            var nlerp = v1.Nlerp(v2, 0.5);
-            Console.WriteLine("Nlerp: " + nlerp);
-        }
-
-        static void BenchNlerp()
-        {
-            var a = new Vector2[BenchSize];
-            var sw = Stopwatch.StartNew();
-            for (int i = 0; i < BenchSize; i++)
             {
                 var v1 = new Vector2(1.0, 1.0);
                 var v2 = new Vector2(1.0, 2.0);
-                var nlerp = v1.Nlerp(v2, 0.5);
-                a[i] = nlerp;
+                var substract = v1.Substract(v2);
+                Console.WriteLine("Substract: " + substract);
             }
-            sw.Stop();
-            Console.WriteLine("Nlerp: " + sw.Elapsed.TotalMilliseconds + "ms");
-        }
 
-        static void TestSlerp()
-        {
-            var v1 = new Vector2(1.0, 0.0);
-            var v2 = new Vector2(0.0, 1.0);
-            var slerp = v1.Slerp(v2, 0.5);
-            Console.WriteLine("Slerp: " + slerp);
-        }
-
-        static void BenchSlerp()
-        {
-            var a = new Vector2[BenchSize];
-            var sw = Stopwatch.StartNew();
-            for (int i = 0; i < BenchSize; i++)
             {
                 var v1 = new Vector2(1.0, 1.0);
                 var v2 = new Vector2(1.0, 2.0);
-                var slerp = v1.Slerp(v2, 0.5);
-                a[i] = slerp;
-            }
-            sw.Stop();
-            Console.WriteLine("Slerp: " + sw.Elapsed.TotalMilliseconds + "ms");
-        }
-
-        static void TestLerp()
-        {
-            var v1 = new Vector2(1.0, 1.0);
-            var v2 = new Vector2(1.0, 2.0);
-            var lerp = v1.Lerp(v2, 0.2);
-            Console.WriteLine("Lerp: " + lerp);
-        }
-
-        static void TestNormalize()
-        {
-            var v = new Vector2(10.0, 0.0);
-            var normalize = v.Normalize();
-            Console.WriteLine("Normalize: " + normalize);
-        }
-
-        static void TestDivide()
-        {
-            {
-                var v1 = new Vector2(1.0, 1.0);
-                var v2 = v1.Divide(2.0);
-                Console.WriteLine("Divide: " + v2);
-            }
-
-            {
-                var v1 = new Vector2(1.0, 1.0);
-                var v2 = v1 / 2.0;
-                Console.WriteLine("Divide: " + v2);
-            }
-        }
-
-        static void TestNearestPointOnLine()
-        {
-            {
-                var a = new Vector2(100.0, 100.0);
-                var b = new Vector2(100.0, 200.0);
-                var p = new Vector2(50.0, 150.0);
-                var nearest = p.NearestPointOnLine(a, b);
-                Console.WriteLine("NearestPointOnLine: " + nearest);
-            }
-
-            {
-                var a = new PointF(100.0f, 100.0f);
-                var b = new PointF(100.0f, 200.0f);
-                var p = new PointF(50.0f, 150.0f);
-                var nearest = VectorUtil.NearestPointOnLine(a, b, p);
-                Console.WriteLine("NearestPointOnLine: " + nearest);
-            }
-        }
-
-        static void TestLengthSquared()
-        {
-            var v = new Vector2(10.0, 0.0);
-            var lengthSquared = v.LengthSquared();
-            Console.WriteLine("LengthSquared: " + lengthSquared);
-        }
-
-        static void TestLength()
-        {
-            var v = new Vector2(10.0, 0.0);
-            var length = v.Length();
-            Console.WriteLine("Length: " + length);
-        }
-
-        static void TestMiddle()
-        {
-            {
-                var a = new Vector2(100.0, 100.0);
-                var b = new Vector2(100.0, 200.0);
-                var middle = a.Middle(b);
-                Console.WriteLine("Middle: " + middle);
-            }
-
-            {
-                var a = new PointF(100.0f, 100.0f);
-                var b = new PointF(100.0f, 200.0f);
-                var p = new PointF();
-                LineUtil.Middle(ref p, a.X, a.Y, b.X, b.Y);
-                Console.WriteLine("Middle: " + p);
-            }
-        }
-
-        static void TestDistance()
-        {
-            {
-                var a = new Vector2(100.0, 100.0);
-                var b = new Vector2(100.0, 200.0);
-                var distance = a.Distance(b);
-                Console.WriteLine("Distance: " + distance);
-            }
-
-            {
-                var a = new PointF(100.0f, 100.0f);
-                var b = new PointF(100.0f, 200.0f);
-                var distance = LineUtil.Distance(a.X, a.Y, b.X, b.Y);
-                Console.WriteLine("Distance: " + distance);
+                var substract = v1 - v2;
+                Console.WriteLine("Substract: " + substract);
             }
         }
 
@@ -445,21 +312,92 @@ namespace MathUtil.Tests
             }
         }
 
-        static void TestSubstract()
+        static void TestMultiply()
         {
             {
                 var v1 = new Vector2(1.0, 1.0);
-                var v2 = new Vector2(1.0, 2.0);
-                var substract = v1.Substract(v2);
-                Console.WriteLine("Substract: " + substract);
+                var v2 = v1.Multiply(2.0);
+                Console.WriteLine("Multiply: " + v2);
             }
 
             {
                 var v1 = new Vector2(1.0, 1.0);
-                var v2 = new Vector2(1.0, 2.0);
-                var substract = v1 - v2;
-                Console.WriteLine("Substract: " + substract);
+                var v2 = v1 * 2.0;
+                Console.WriteLine("Multiply: " + v2);
             }
+        }
+
+        static void BenchMultiply()
+        {
+            var a = new Vector2[BenchSize];
+            var sw = Stopwatch.StartNew();
+            for (int i = 0; i < BenchSize; i++)
+            {
+                var v1 = new Vector2(1.0, 1.0);
+                var v2 = v1.Multiply(2.0);
+                a[i] = v2;
+            }
+            sw.Stop();
+            Console.WriteLine("Multiply: " + sw.Elapsed.TotalMilliseconds + "ms");
+        }
+
+        static void TestDivide()
+        {
+            {
+                var v1 = new Vector2(1.0, 1.0);
+                var v2 = v1.Divide(2.0);
+                Console.WriteLine("Divide: " + v2);
+            }
+
+            {
+                var v1 = new Vector2(1.0, 1.0);
+                var v2 = v1 / 2.0;
+                Console.WriteLine("Divide: " + v2);
+            }
+        }
+
+        static void TestDot()
+        {
+            var v1 = new Vector2(1.0, 1.0);
+            var v2 = new Vector2(1.0, 2.0);
+            double dot = v1.Dot(v2);
+            Console.WriteLine("Dot: " + dot);
+        }
+
+        static void BenchDot()
+        {
+            var a = new double[BenchSize];
+            var sw = Stopwatch.StartNew();
+            for (int i = 0; i < BenchSize; i++)
+            {
+                var v1 = new Vector2(1.0, 1.0);
+                var v2 = new Vector2(1.0, 2.0);
+                double dot = v1.Dot(v2);
+                a[i] = dot;
+            }
+            sw.Stop();
+            Console.WriteLine("Dot: " + sw.Elapsed.TotalMilliseconds + "ms");
+        }
+
+        static void TestLength()
+        {
+            var v = new Vector2(10.0, 0.0);
+            var length = v.Length();
+            Console.WriteLine("Length: " + length);
+        }
+
+        static void TestLengthSquared()
+        {
+            var v = new Vector2(10.0, 0.0);
+            var lengthSquared = v.LengthSquared();
+            Console.WriteLine("LengthSquared: " + lengthSquared);
+        }
+
+        static void TestNormalize()
+        {
+            var v = new Vector2(10.0, 0.0);
+            var normalize = v.Normalize();
+            Console.WriteLine("Normalize: " + normalize);
         }
 
         static void TestProject()
@@ -494,56 +432,112 @@ namespace MathUtil.Tests
             Console.WriteLine("Project: " + sw.Elapsed.TotalMilliseconds + "ms");
         }
 
-        static void TestMultiply()
+        static void TestLerp()
         {
-            {
-                var v1 = new Vector2(1.0, 1.0);
-                var v2 = v1.Multiply(2.0);
-                Console.WriteLine("Multiply: " + v2);
-            }
-
-            {
-                var v1 = new Vector2(1.0, 1.0);
-                var v2 = v1 * 2.0;
-                Console.WriteLine("Multiply: " + v2);
-            }
+            var v1 = new Vector2(1.0, 1.0);
+            var v2 = new Vector2(1.0, 2.0);
+            var lerp = v1.Lerp(v2, 0.2);
+            Console.WriteLine("Lerp: " + lerp);
         }
 
-        static void BenchMultiply()
+        static void TestSlerp()
+        {
+            var v1 = new Vector2(1.0, 0.0);
+            var v2 = new Vector2(0.0, 1.0);
+            var slerp = v1.Slerp(v2, 0.5);
+            Console.WriteLine("Slerp: " + slerp);
+        }
+
+        static void BenchSlerp()
         {
             var a = new Vector2[BenchSize];
             var sw = Stopwatch.StartNew();
             for (int i = 0; i < BenchSize; i++)
             {
                 var v1 = new Vector2(1.0, 1.0);
-                var v2 = v1.Multiply(2.0);
-                a[i] = v2;
+                var v2 = new Vector2(1.0, 2.0);
+                var slerp = v1.Slerp(v2, 0.5);
+                a[i] = slerp;
             }
             sw.Stop();
-            Console.WriteLine("Multiply: " + sw.Elapsed.TotalMilliseconds + "ms");
+            Console.WriteLine("Slerp: " + sw.Elapsed.TotalMilliseconds + "ms");
         }
 
-        static void TestDot()
+        static void TestNlerp()
         {
-            var v1 = new Vector2(1.0, 1.0);
-            var v2 = new Vector2(1.0, 2.0);
-            double dot = v1.Dot(v2);
-            Console.WriteLine("Dot: " + dot);
+            var v1 = new Vector2(1.0, 0.0);
+            var v2 = new Vector2(0.0, 1.0);
+            var nlerp = v1.Nlerp(v2, 0.5);
+            Console.WriteLine("Nlerp: " + nlerp);
         }
 
-        static void BenchDot()
+        static void BenchNlerp()
         {
-            var a = new double[BenchSize];
+            var a = new Vector2[BenchSize];
             var sw = Stopwatch.StartNew();
             for (int i = 0; i < BenchSize; i++)
             {
                 var v1 = new Vector2(1.0, 1.0);
                 var v2 = new Vector2(1.0, 2.0);
-                double dot = v1.Dot(v2);
-                a[i] = dot;
+                var nlerp = v1.Nlerp(v2, 0.5);
+                a[i] = nlerp;
             }
             sw.Stop();
-            Console.WriteLine("Dot: " + sw.Elapsed.TotalMilliseconds + "ms");
+            Console.WriteLine("Nlerp: " + sw.Elapsed.TotalMilliseconds + "ms");
+        }
+
+        static void TestDistance()
+        {
+            {
+                var a = new Vector2(100.0, 100.0);
+                var b = new Vector2(100.0, 200.0);
+                var distance = a.Distance(b);
+                Console.WriteLine("Distance: " + distance);
+            }
+
+            {
+                var a = new PointF(100.0f, 100.0f);
+                var b = new PointF(100.0f, 200.0f);
+                var distance = LineUtil.Distance(a.X, a.Y, b.X, b.Y);
+                Console.WriteLine("Distance: " + distance);
+            }
+        }
+
+        static void TestMiddle()
+        {
+            {
+                var a = new Vector2(100.0, 100.0);
+                var b = new Vector2(100.0, 200.0);
+                var middle = a.Middle(b);
+                Console.WriteLine("Middle: " + middle);
+            }
+
+            {
+                var a = new PointF(100.0f, 100.0f);
+                var b = new PointF(100.0f, 200.0f);
+                var p = new PointF();
+                LineUtil.Middle(ref p, a.X, a.Y, b.X, b.Y);
+                Console.WriteLine("Middle: " + p);
+            }
+        }
+
+        static void TestNearestPointOnLine()
+        {
+            {
+                var a = new Vector2(100.0, 100.0);
+                var b = new Vector2(100.0, 200.0);
+                var p = new Vector2(50.0, 150.0);
+                var nearest = p.NearestPointOnLine(a, b);
+                Console.WriteLine("NearestPointOnLine: " + nearest);
+            }
+
+            {
+                var a = new PointF(100.0f, 100.0f);
+                var b = new PointF(100.0f, 200.0f);
+                var p = new PointF(50.0f, 150.0f);
+                var nearest = VectorUtil.NearestPointOnLine(a, b, p);
+                Console.WriteLine("NearestPointOnLine: " + nearest);
+            }
         }
     }
 }
